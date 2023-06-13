be6cb7e7c1dc28a354f3b4ac3ce98565712f3e34
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2023-06-13 08:16:29 -0400

    Add src files

---
+++ src/add.js
@@ -0,0 +1,29 @@
+class AddToDo {
+    constructor() {
+      this.toDo = JSON.parse(localStorage.getItem('toDo')) || [];
+      this.form = document.getElementById('submit-form');
+      this.userInput = document.getElementById('add-list');
+    }
+  
+    addToDo(description) {
+      const addToDoObj = { description, completed: false, num: this.toDo.length + 1 };
+      this.toDo.push(addToDoObj);
+      localStorage.setItem('toDo', JSON.stringify(this.toDo));
+    }
+  
+    activ() {
+      this.userInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          this.addToDo(this.userInput.value.trim());
+          window.location.reload();
+          this.form.reset();
+        }
+      });
+    }
+  }
+  
+  export default function activ() {
+    const newToDo = new AddToDo();
+    newToDo.activ();
+  }
